144359c6506e4645db45e6355f41e0ef05d1b2f3
Kouji Matsui <k@kekyo.net>
2022-11-13 21:45:36 +0900

    Updated version.

--- startup.sh
+++ startup.sh
@@ -5,8 +5,8 @@ sudo apt-get upgrade -y
 
 cd /a
 
-curl -o actions-runner-linux-x64-2.293.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.293.0/actions-runner-linux-x64-2.293.0.tar.gz
-tar zxvf actions-runner-linux-x64-2.293.0.tar.gz
+curl -o actions-runner-linux-x64-2.299.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.293.0/actions-runner-linux-x64-2.299.1.tar.gz
+tar zxvf actions-runner-linux-x64-2.299.1.tar.gz
 
 ./config.sh --unattended --replace --url $GA_URL --token $GA_TOKEN --name $GA_NAME
 
